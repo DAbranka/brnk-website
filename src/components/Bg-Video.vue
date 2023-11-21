c2fc6b8a8fe9f54c44feb61dd603a1cff2b1b77f
DAbranka <d.abranka@hotmail.com>
2023-11-21 15:50:03 +0100

    Hero Video BG added

---
+++ src/components/Bg-Video.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <video autoplay loop>
+            <source src="@/assets/video/BRNK_FLAG_Post_Prod.mp4" type="video/mp4">
+        </video>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/The-Hero.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <BgVideo />
+    </div>
+</template>
+
+<script setup>
+    import BgVideo from './Bg-Video.vue';
+</script>
+
+<style scoped>
+
+</style>
